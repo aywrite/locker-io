f5fc36660958ba556e90353acf3bd9032d686172
awright502 <andrew.wright502@gmail.com>
2017-05-04 21:37:24 +1000

    Add basic command line argument parsing with clap.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+extern crate clap;
 extern crate scytale;
+
+use clap::{Arg, App, SubCommand};
 
 use scytale::rot13::rot13;
 use std::io;
@@ -11,15 +14,34 @@ use std::io::prelude::*;
 // - Implement cli flags/options
 
 fn main() {
-    
-    //read from stdin until EOF recived
-    let mut plaintext = String::new();
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        plaintext = plaintext + line.unwrap().as_str() + "\n";
-    }
+    let matches = App::new("cryptio")
+                          .version("0.0.1")
+                          .author("Andrew W. <andrew.wright@gmail.com>")
+                          .about("Utility for encrypting/decrypting")
+                          .arg(Arg::with_name("input")
+                               .index(1)
+                               .help("Text to be encrypted"))
+                          .get_matches();
+
+    //TODO seems like too many conversions between
+    //String and &str see also input to encrypt below
+    let input = if matches.is_present("input") {
+        matches.value_of("input").unwrap().to_string()
+    } else {
+        get_input_from_stdin()
+    };
 
     //print encrypted text
-    let ciphertext = rot13(plaintext.as_str());
+    let ciphertext = rot13(input.as_str());
     println!("{}", ciphertext);
+}
+
+fn get_input_from_stdin() -> String {
+    //read from stdin until EOF recieved
+    let mut input = String::new();
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        input = input + line.unwrap().as_str() + "\n";
+    }
+    input
 }
